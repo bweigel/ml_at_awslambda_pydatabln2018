1ddee56e5d58aafebb7544c9051744c1e0aced49
bweigel <benjamin.weigel@europace.de>
2018-06-13 16:28:08 +0200

    updates Makefile - adds deploy-model target

--- tutorial/training/deploy_model.sh
+++ tutorial/training/deploy_model.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+BASEDIR=$( dirname "${BASH_SOURCE[0]}" )
+
 set -euo pipefail
 
 read -e -p "Enter bucket name: " -i "dreigelb.public" BUCKET
@@ -7,4 +9,4 @@ read -e -p "Enter object prefix: " -i "pydatabln2018" PREFIX
 
 set -x
 
-aws s3 cp --recursive ./models s3://${BUCKET}/${PREFIX}/
+aws s3 cp --recursive ${BASEDIR}/models s3://${BUCKET}/${PREFIX}/

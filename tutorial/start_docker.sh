015f6c2f196a39dbee7dcc3d9085e47ca6086fa3
bweigel <benjamin.weigel@europace.de>
2018-06-18 22:51:57 +0200

    fixes bug in serverless deployment see: https://forum.serverless.com/t/profile-default-does-not-exist/3033

---
+++ tutorial/start_docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo ${AWS_PROFILE:=default}
+
+export AWS_ACCESS_KEY_ID=$(aws --profile ${AWS_PROFILE} configure get aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(aws --profile ${AWS_PROFILE} configure get aws_secret_access_key)
+
+docker run --rm -ti -v $(pwd):/workd -w /workd \
+    -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+    -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} bweigel/ml_at_awslambda_pydatabln2018:latest /bin/bash

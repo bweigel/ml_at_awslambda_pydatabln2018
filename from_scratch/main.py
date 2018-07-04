63daf33cebb62236b2c2e5173e80d92bdcc0be61
bweigel <benjamin.weigel@europace.de>
2018-07-04 17:03:47 +0200

    adds workshop solution

--- from_scratch/main.py
+++ from_scratch/main.py
@@ -1,6 +1,30 @@
+from io import BytesIO
+from sklearn.externals import joblib
+
+import boto3
+
+
+def get_model():
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('pydata.workshop.2018')
+
+    with BytesIO() as nbmodel:
+        bucket.download_fileobj(Key="models/pipeline.pkl", Fileobj=nbmodel)
+        pipe = joblib.load(nbmodel)
+
+    return pipe
+
+
+def predict(data):
+    model = get_model()
+    data = [data]
+    result = model.predict(data)
+    return result.tolist()[0]
 
 
 def lambda_handler(event, context):
-    result = ...
+    data = event["body"]
+    result = predict(data)
+    print(f"The result of the classifaction is {result}")
     return {"statusCode": 200,
             "body": result}

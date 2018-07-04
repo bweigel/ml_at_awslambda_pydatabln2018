552dbfd87496df436262da86f27988945ab89405
bweigel <benjamin.weigel@europace.de>
2018-07-04 15:57:56 +0200

    use pipeline to string together vectorizer & classifier. use multinomial bayes for sparse matrix compatibility

--- tutorial/training/train.py
+++ tutorial/training/train.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+from sklearn.base import TransformerMixin
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.metrics import accuracy_score
 from sklearn.externals import joblib
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import FunctionTransformer
 
 df = pd.read_csv('./data/spam.csv', encoding='latin-1')[["v1", "v2"]]
 
@@ -30,7 +33,7 @@ Train Naive Bayes Classifier
 data_train_transformed = vectorizer.transform(data_train).toarray()
 
 # fit gaussian naive bayes classifier
-clf = GaussianNB()
+clf = MultinomialNB()
 clf.fit(data_train_transformed, labels_train)
 
 joblib.dump(clf, './models/naive_bayes_clf.pkl')
@@ -42,3 +45,15 @@ data_test_transformed = vectorizer.transform(data_test).toarray()
 predictions = clf.predict(data_test_transformed)
 
 print(f"The accuracy against the test set is {accuracy_score(labels_test, predictions)}")
+
+
+"""
+Create Pipeline
+"""
+
+pipe = make_pipeline(vectorizer, clf)
+pipe.fit(data_train, labels_train)
+joblib.dump(pipe, './models/pipeline.pkl')
+
+pipeline_predictions = pipe.predict(data_test)
+print(f"The accuracy against the test set is {accuracy_score(labels_test, pipeline_predictions)}")

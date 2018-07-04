d4d62bb41e8ebe4b5a56d9ce614251d06098d7a1
bweigel <benjamin.weigel@europace.de>
2018-07-04 17:58:41 +0200

    updates start_docker skript

--- start_docker.sh
+++ start_docker.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
-echo ${AWS_PROFILE:=default}
-
-export AWS_ACCESS_KEY_ID=$(aws --profile ${AWS_PROFILE} configure get aws_access_key_id)
-export AWS_SECRET_ACCESS_KEY=$(aws --profile ${AWS_PROFILE} configure get aws_secret_access_key)
+if [[ -z "${AWS_ACCESS_KEY_ID:-}" ]]; then
+    echo "AWS_ACCESS_KEY_ID not set in environment."
+    echo "Getting AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from profile \"${AWS_PROFILE:=default}\"..."
+    export AWS_ACCESS_KEY_ID=$(aws --profile ${AWS_PROFILE} configure get aws_access_key_id)
+    export AWS_SECRET_ACCESS_KEY=$(aws --profile ${AWS_PROFILE} configure get aws_secret_access_key)
+fi
 
 docker pull bweigel/ml_at_awslambda_pydatabln2018_autobuild:latest
 docker run --rm -ti -v $(pwd)/from_scratch:/from_scratch -w /from_scratch \

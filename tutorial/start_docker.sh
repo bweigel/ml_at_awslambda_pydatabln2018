254f3af0e60b13a47efa91981b3b8d24afcf3442
bweigel <benjamin.weigel@europace.de>
2018-07-04 16:02:16 +0200

    updates dockerfile

--- tutorial/start_docker.sh
+++ tutorial/start_docker.sh
@@ -7,6 +7,7 @@ echo ${AWS_PROFILE:=default}
 export AWS_ACCESS_KEY_ID=$(aws --profile ${AWS_PROFILE} configure get aws_access_key_id)
 export AWS_SECRET_ACCESS_KEY=$(aws --profile ${AWS_PROFILE} configure get aws_secret_access_key)
 
-docker run --rm -ti -v $(pwd)/from_scratch:/workd -w /workd \
+docker pull bweigel/ml_at_awslambda_pydatabln2018_autobuild:latest
+docker run --rm -ti -v $(pwd)/from_scratch:/from_scratch -w /from_scratch \
     -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
     -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} bweigel/ml_at_awslambda_pydatabln2018_autobuild:latest /bin/bash
